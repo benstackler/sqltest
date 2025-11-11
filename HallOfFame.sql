00819890780b72a7ef8811d602670f6d41090669
benstackler <ben.stackler@gmail.com>
2025-11-11 10:41:59 -0500

    Update HallOfFame.sql

--- HallOfFame.sql
+++ HallOfFame.sql
@@ -1,1 +1,12 @@
+# Creating Necessary Table - Master
+
+ Create table master (playerID text, birthyear int, birthmonth int, birthday int, birthcountry text, 
+  birthstate text, birthcity text, deathyear int, deathmonth int, deathday int, deathcountry text, deathstate text, 
+  deathcity text, namefirst text, namelast text, namegiven text, weight int, height int, bats text, throws text, debut date, 
+  finalgame date, retroid text, bbrefid text);
+
+# Checking Your Import
+
+  Select * from master;
+
 select * from hof left join players on hof.playerid = players.playerid where hof.inducted = 'Y' and yearid > 1990 order by yearid desc;
